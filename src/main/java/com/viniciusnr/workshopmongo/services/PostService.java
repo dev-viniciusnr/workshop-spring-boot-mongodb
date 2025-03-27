dd8c3135b67116184cf99e6a63dfd46852ba9d7c
Vinícius Nonato <v245394@dac.unicamp.br>
2025-03-27 20:24:55 -0300

    Getting post by id

---
+++ src/main/java/com/viniciusnr/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.viniciusnr.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniciusnr.workshopmongo.domain.Post;
+import com.viniciusnr.workshopmongo.repository.PostRepository;
+import com.viniciusnr.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    if (user.isEmpty()) {
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+	    return user.get();
+	}
+
+	
+}
