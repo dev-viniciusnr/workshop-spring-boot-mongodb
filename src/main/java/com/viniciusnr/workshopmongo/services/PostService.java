f91c8d34e64bd15dcbc0cd64368add14643a7f0e
Vinícius Nonato <v245394@dac.unicamp.br>
2025-03-28 20:03:30 -0300

    simple queries with querys methods

--- src/main/java/com/viniciusnr/workshopmongo/services/PostService.java
+++ src/main/java/com/viniciusnr/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.viniciusnr.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,8 @@ public class PostService {
 	    return user.get();
 	}
 
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContaining(text);
+	}
 	
 }
